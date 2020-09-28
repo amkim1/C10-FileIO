5ffcf1f9b686c263dc8ee12810784615af6ad434
Alex Kim <amkim@mymail.mines.edu>
2020-09-28 15:06:57 -0600

    Initial  classes

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+
+public class Student {
+	private String name;
+	private int score;
+	
+	public Student(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+	
+	//public void toString()  {
+		
+	//}
+}
