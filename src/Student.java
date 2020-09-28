ba5116b1b480a2113d3d9bacec2605c5fee49acc
Mohammed Alnasser <w-r-t-2002@hotmail.com>
2020-09-28 16:44:52 -0600

    added comments for Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,18 +1,40 @@
+/**
+ * 
+ * @author Alex Kim
+ * @author Mohammed Alnasser
+ *
+ */
 
+/**
+ * Student class which will store students name and their score
+ */
 public class Student implements Comparable<Student> {
 	private String name;
 	private int score;
 	
+	/**
+	 * constructor for student
+	 * @param name
+	 * @param score
+	 */
 	public Student(String name, int score) {
 		this.name = name;
 		this.score = score;
 	}
 
+	/**
+	 * compareTo function to compare the names of the students
+	 * @param otherStudent the other student to compare to
+	 */
 	@Override
-	public int compareTo(Student o) {
-		return name.compareTo(o.name);
+	public int compareTo(Student otherStudent) {
+		return name.compareTo(otherStudent.name);
 	}
 	
+	/**
+	 * toString function to print the student info
+	 */
+	@Override
 	public String toString()  {
 		return name + " " + score;
 	}
